562a2494fc1d40f9e2aea94fe2a9f2b2b650e0b7
Jeremy Banks <_@jeremy.ca>
2022-07-14 03:19:08 +0000

    r2

--- src/main.rs
+++ src/main.rs
@@ -14,32 +14,12 @@ fn main() {
         name: "John".to_string(),
         id: 1,
     };
-    let bytes = postcard::to_stdvec(&foo).unwrap();
+    let args = std::env::vars().collect::<Vec<_>>();
+    let bytes = postcard::to_stdvec(&args).unwrap();
     println!("{}", byte_literal(&bytes, 0));
 
-    let bytes = (0..=0xFF).into_iter().collect::<Vec<u8>>();
-    println!("{}", byte_literal(&bytes, 0));
-
-    let x = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\
-                          \x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !\x22#$%&'()*+,-./0123\
-                          456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\x5C]^_`abcdefghijklmnopqrstuvwxyz{\
-                          |}~\x7F\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\
-                          \x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\
-                          \xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\
-                          \x20       \xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\
-                          \xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\
-                          \xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\
-                          \xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF";
-
-    println!("{}", byte_literal(x, 0));
-    println!("  {}", byte_literal(x, 2));
-    println!("    {}", byte_literal(x, 4));
-
-    let x = (0..=0xFF).into_iter().map(|_| 0).collect::<Vec<u8>>();
-
-    println!("{}", byte_literal(&*x, 0));
-    println!("  {}", byte_literal(&*x, 2));
-    println!("    {}", byte_literal(&*x, 4));
+    panic!("you can't use a formatting that's not rustfmt-friendly, eh?");
+    // might be good enough to get started, though
 }
 
 fn byte_literal(bytes: &[u8], indent: usize) -> String {
@@ -63,17 +43,18 @@ fn byte_literal(bytes: &[u8], indent: usize) -> String {
                     write!(s, "\\n\\\n  {}", " ".repeat(indent)).unwrap();
                     last_newline_index = s.len() as i64;
                 } else {
-                    write!(s, "\\x10").unwrap();
+                    write!(s, "\\x0A").unwrap();
                 }
                 previous_was_printable = false;
             }
             // leading spaces (following a backslashed-newline) would be trimmed unless encoded
             b' ' => {
-                if line_broken {
+                if line_broken || !previous_was_printable {
                     write!(s, "\\x20").unwrap();
                 } else {
                     write!(s, " ").unwrap();
                 }
+                previous_was_printable = true;
             }
             // most ascii printable characters are left as-is
             b'!' | b'#'..=b'[' | b']'..=b'~' => {
