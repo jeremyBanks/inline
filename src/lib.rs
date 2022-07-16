a8106a3d87d0dff3c34bb7757de05e42ea7ccc1d
Jeremy Banks <_@jeremy.ca>
2022-07-16 06:47:53 +0000

    r9

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,33 @@
 #![doc = include_str!("../README.md")]
 //!
 #![doc = document_features::document_features!()]
-#![cfg_attr(debug_assertions, allow(unused))]
+// We wan to warn about these...
+#![warn(
+    unused_qualifications,
+    unused_macro_rules,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_crate_dependencies,
+    missing_docs
+)]
+// ...but not in debug builds or the IDE, that's too noisy.
+// So we skip them except in test, doc, and release builds.
+#![cfg_attr(
+    all(debug_assertions, not(all(any(test, doc), not(feature = "__all__")))),
+    allow(
+        unused,
+        unused_crate_dependencies,
+        missing_docs,
+        unused_import_braces,
+        unused_macro_rules,
+        unused_qualifications
+    )
+)]
+// Note that Cargo ignores all of these built-in lints for dependencies, and
+// this crate is a library, not a binary. So although we promote warnings
+// to errors, this is only relevant for when we build or run this as
+// `--release` directly, during development and testing, not for our users.
+#![cfg_attr(not(debug_assertions), deny(warnings))]
 
 mod literal;
 pub use self::literal::*;
@@ -31,8 +57,3 @@ pub use self::litter_handle::*;
 mod serde;
 #[cfg(feature = "serde")]
 pub use self::serde::*;
-
-#[cfg(feature = "rkyv")]
-mod rkyv;
-#[cfg(feature = "rkyv")]
-pub use self::rkyv::*;
